17359fec098b0122e8178a8fff82925aee3ecd6c
West14 <30056636+West14@users.noreply.github.com>
2019-12-18 20:40:38 +0200

    Remove sendConversation method.

--- XF/Service/User/Upgrade.php
+++ XF/Service/User/Upgrade.php
@@ -10,37 +10,32 @@ class Upgrade extends XFCP_Upgrade
 
         if ($parent instanceof \XF\Entity\UserUpgradeActive)
         {
-            $this->sendConversation();
+            /** @var \XF\Service\Conversation\Creator $creator */
+            $creator = $this->service(
+                'XF:Conversation\Creator',
+                $starterUser = $this->findOne('XF:User', [
+                    'username' => $this->app->options()->wUucStarterUser
+                ])
+            );
+            $creator->setRecipientsTrusted($this->user);
+            $creator->setIsAutomated();
+            $creator->setOptions([
+                'open_invite' => false
+            ]);
+            $creator->setContent(
+                \XF::phrase('w_uuc_conversation_title')->render(),
+                \XF::phrase('w_uuc_conversation_message', [
+                    'username' => $this->user->username,
+                    'upgrade_title' => $this->userUpgrade->title,
+                    'end_date' => $this->endDate
+                        ? $this->app->language($this->user->language_id)->dateTime($this->endDate)
+                        : \XF::phrase('never')->render('raw')
+                ])->render('raw')
+            );
+
+            $creator->save();
         }
 
         return $parent;
-    }
-
-    protected function sendConversation()
-    {
-        /** @var \XF\Service\Conversation\Creator $creator */
-        $creator = $this->service(
-            'XF:Conversation\Creator',
-            $starterUser = $this->findOne('XF:User', [
-                'username' => $this->app->options()->wUucStarterUser
-            ])
-        );
-        $creator->setRecipientsTrusted($this->user);
-        $creator->setIsAutomated();
-        $creator->setOptions([
-            'open_invite' => false
-        ]);
-        $creator->setContent(
-            \XF::phrase('w_uuc_conversation_title')->render(),
-            \XF::phrase('w_uuc_conversation_message', [
-                'username' => $this->user->username,
-                'upgrade_title' => $this->userUpgrade->title,
-                'end_date' => $this->endDate
-                    ? $this->app->language($this->user->language_id)->dateTime($this->endDate)
-                    : \XF::phrase('never')->render()
-            ])->render()
-        );
-        
-        $creator->save();
     }
 }
